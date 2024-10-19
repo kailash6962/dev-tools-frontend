74c527763ed1e6c2d5f735b45916cb02e4122508
Kailash-Ubuntu <kailashk6962@gmail.com>
2024-10-19 22:52:47 +0530

    dashboard

--- src/utils/common.js
+++ src/utils/common.js
@@ -5,3 +5,10 @@ export function objectToSelectOptions(data,key,value){
   }));
   return result;
 }
+export function sumColumn(arr, columnName) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i][columnName] || 0; // Assuming missing values are treated as 0
+  }
+  return Math.round(sum);
+}
